a79ba45706d6b3c080d8888e915222be91aec16d
Tokoshima <louwster3000@gmail.com>
2021-02-07 17:29:52 +0200

    Finished qt5 app. handling

--- LuCert.py
+++ LuCert.py
@@ -1,6 +1,7 @@
+from qtUI import Ui_MainWindow
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from openpyxl import Workbook, load_workbook
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import gettex
 import docx
@@ -10,63 +11,75 @@ import sys,os
 
 
 class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow,self).__init__()
-        self.initUI()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #self.initUI()
 
-    def button_clicked(self):
-        self.label.setText("you pressed the button")
-        self.update()
+        self.uimw = Ui_MainWindow
+        self.uimw.setupUi(self)
 
-        os.chdir(os.path.abspath('Lists'))
-        wb = load_workbook("List.xlsx")
-        #print("Cek1")
-        source = wb["Sheet1"]
-        for cell in source['%c' % col]:
-            #print("Before chdir masterdoc")
-            #print(os.getcwd())
+    # def button_clicked(self):
+    #     self.label.setText("you pressed the button")
+    #     self.update()
+    #
+    #     os.chdir(os.path.abspath('Lists'))
+    #     wb = load_workbook("List.xlsx")
+    #     #print("Cek1")
+    #     source = wb["Sheet1"]
+    #     for cell in source['%c' % col]:
+    #         #print("Before chdir masterdoc")
+    #         #print(os.getcwd())
+    #
+    #         os.chdir(os.path.abspath('..'))
+    #         #print("After chdir masterdoc")
+    #         #print(os.getcwd())
+    #         print(cell.value)
+    #         name =cell.value
+    #         doc = docx.Document('masterDoc.docx')
+    #         p1 = doc.add_paragraph('%s' % name)
+    #         p1.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    #
+    #
+    #         if os.path.exists(os.path.abspath("Certs")):
+    #             os.chdir(os.path.abspath("Certs"))
+    #             doc.save('%s.docx' % cell.value)
+    #         else:
+    #             print('failed')
+    #
+    #
+    # def initUI(self):
+    #     self.setGeometry(200, 200, 300, 300)
+    #     self.setWindowTitle("Tech With Tim")
+    #
+    #     self.label = QtWidgets.QLabel(self)
+    #     self.label.setText("my first label!")
+    #
+    #     self.label.move(50,50)
+    #     self.b1 = QtWidgets.QPushButton(self)
+    #     self.b1.setText("click me!")
+    #     self.b1.clicked.connect(self.button_clicked)
+    #
+    # def update(self):
+    #     self.label.adjustSize()
+# 
+# def window():
+#     app = QApplication(sys.argv)
+#     win = MyWindow()
+#     win.show()
+#
+#
+#     sys.exit(app.exec_())
+#
+# window()
 
-            os.chdir(os.path.abspath('..'))
-            #print("After chdir masterdoc")
-            #print(os.getcwd())
-            print(cell.value)
-            name =cell.value
-            doc = docx.Document('masterDoc.docx')
-            p1 = doc.add_paragraph('%s' % name)
-            p1.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-
-
-            if os.path.exists(os.path.abspath("Certs")):
-                os.chdir(os.path.abspath("Certs"))
-                doc.save('%s.docx' % cell.value)
-            else:
-                print('failed')
-
-
-    def initUI(self):
-        self.setGeometry(200, 200, 300, 300)
-        self.setWindowTitle("Tech With Tim")
-
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText("my first label!")
-
-        self.label.move(50,50)
-        self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("click me!")
-        self.b1.clicked.connect(self.button_clicked)
-
-    def update(self):
-        self.label.adjustSize()
-
-def window():
-    app = QApplication(sys.argv)
-    win = MyWindow()
-    win.show()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
-
-window()
-
-
 
 
 
