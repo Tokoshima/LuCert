6472ffc208e8352b75b8c523a631d16e0c2ee788
Tokoshima <louwster3000@gmail.com>
2021-02-07 05:33:52 +0200

    Added basic PyQt5 module and functionality

--- LuCert.py
+++ LuCert.py
@@ -1,49 +1,78 @@
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+from openpyxl import Workbook, load_workbook
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import gettex
 import docx
 import openpyxl
 import sys,os
 
-from openpyxl import Workbook, load_workbook
-os.chdir(os.path.abspath('Lists'))
-wb = load_workbook("List.xlsx")
-#print("Cek1")
-source = wb["Sheet1"]
-for cell in source['A']:
-    #print("Before chdir masterdoc")
-    #print(os.getcwd())
-    #print(__file__)
-    #print(os.path.join(os.path.dirname(__file__), '..'))
-    #print(os.path.dirname(os.path.realpath(__file__)))
-    #print(os.path.abspath(os.path.dirname(__file__)))
-
-    os.chdir(os.path.abspath('..'))
-    #print("After chdir masterdoc")
-    #print(os.getcwd())
-    #print(cell.value)
-    name =cell.value
-    doc = docx.Document('masterDoc.docx')
-    p1 = doc.add_paragraph('%s' % name)
-    p1.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    
-    #os.chdir('~/Documents/LuCert/Certs')
-    #sys.path.append('/Documents/LuCert/Certs')
-    #print(os.path.abspath("Certs"))
-    
-    if os.path.exists(os.path.abspath("Certs")):
-        os.chdir(os.path.abspath("Certs"))
-        doc.save('%s.docx' % cell.value)
-    else:
-        print('failed')
-   #doc.save('%s.docx' % cell.value)
-   # print(gettex.getText('/Documents/LuCert/Certs/'+ '%s.docx' % cell.value))
- 
 
 
-#name =""
-#doc = docx.Document('masterDoc.docx')
-#p1 = doc.add_paragraph('%s' % name)
-#p1.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-#doc.save('Out.docx')
-#print(gettex.getText('Out.docx'))
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow,self).__init__()
+        self.initUI()
 
+    def button_clicked(self):
+        self.label.setText("you pressed the button")
+        self.update()
+
+        os.chdir(os.path.abspath('Lists'))
+        wb = load_workbook("List.xlsx")
+        #print("Cek1")
+        source = wb["Sheet1"]
+        for cell in source['%c' % col]:
+            #print("Before chdir masterdoc")
+            #print(os.getcwd())
+
+            os.chdir(os.path.abspath('..'))
+            #print("After chdir masterdoc")
+            #print(os.getcwd())
+            print(cell.value)
+            name =cell.value
+            doc = docx.Document('masterDoc.docx')
+            p1 = doc.add_paragraph('%s' % name)
+            p1.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+
+            if os.path.exists(os.path.abspath("Certs")):
+                os.chdir(os.path.abspath("Certs"))
+                doc.save('%s.docx' % cell.value)
+            else:
+                print('failed')
+
+
+    def initUI(self):
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Tech With Tim")
+
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("my first label!")
+
+        self.label.move(50,50)
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("click me!")
+        self.b1.clicked.connect(self.button_clicked)
+
+    def update(self):
+        self.label.adjustSize()
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+window()
+
+
+
+
+
+    #name =""
+    #doc = docx.Document('masterDoc.docx')
+    #p1 = doc.add_paragraph('%s' % name)
+    #p1.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    #doc.save('Out.docx')
+    #print(gettex.getText('Out.docx'))
