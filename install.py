e668d27fff56c06822680411a6949227fd8e289a
Tokoshima <louwster3000@gmail.com>
2021-02-10 17:46:21 +0200

    install fix

--- install.py
+++ install.py
@@ -1,8 +1,15 @@
 import subprocess
 import sys
+#subprocess.check_call([sys.executable, "-m", "pip", "install", "manim"])
+subprocess.check_call([sys.executable, "-m", "pip", "install","openpyxl"])
+subprocess.check_call([sys.executable, "-m", "pip", "install","python-docx"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "docx"])
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install","openpyxl", package])
-    subprocess.check_call([sys.executable, "-m", "pip", "install","python-docx", package])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "docx",package])
-    subprocess.check_call([sys.executable, "python3", "-m", "pip", "install", "PyQt5", package])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5"])
+# def install(package):
+#     subprocess.check_call([sys.executable, "-m", "pip3", "install", "manim",package])
+#     subprocess.check_call([sys.executable, "-m", "pip", "install","openpyxl", package])
+#     subprocess.check_call([sys.executable, "-m", "pip", "install","python-docx", package])
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", "docx",package])
+#
+#     subprocess.check_call([sys.executable, "python3", "-m", "pip", "install", "PyQt5", package])
