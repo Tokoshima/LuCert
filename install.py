9dafd29974d5939f33364ffb4cbcb57ec01a1b42
Tokoshima <louwster3000@gmail.com>
2021-02-10 15:49:20 +0200

    install.py changes

--- install.py
+++ install.py
@@ -2,4 +2,7 @@ import subprocess
 import sys
 
 def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    subprocess.check_call([sys.executable, "-m", "pip", "install","openpyxl", package])
+    subprocess.check_call([sys.executable, "-m", "pip", "install","python-docx", package])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "docx",package])
+    subprocess.check_call([sys.executable, "python3", "-m", "pip", "install", "PyQt5", package])
